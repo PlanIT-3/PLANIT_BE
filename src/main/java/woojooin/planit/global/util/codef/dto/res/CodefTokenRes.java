83245edfdb417fa81e272bacbcda4a292aa19301
greensnapback0229 <smdmim@gmail.com>
2025-07-28 17:23:55 +0900

    feat: RestTemplate 생성자 주입으로 받오도록 변경

--- src/main/java/woojooin/planit/global/util/codef/dto/res/CodefTokenRes.java
+++ src/main/java/woojooin/planit/global/util/codef/dto/res/CodefTokenRes.java
@@ -1,12 +1,10 @@
 package woojooin.planit.global.util.codef.dto.res;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record CodefTokenRes(
-	@JsonProperty("access_token") String accessToken,
-	@JsonProperty("token_type") String tokenType,
-	@JsonProperty("expires_in") Integer expiresIn,
-	@JsonProperty("scope") String scope
+	String accessToken,
+	String tokenType,
+	Integer expiresIn,
+	String scope
 ) {
 }
 
