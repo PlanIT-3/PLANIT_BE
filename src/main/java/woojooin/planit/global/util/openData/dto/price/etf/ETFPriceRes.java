c0fc26af29557498e1700d57af11a5699c39459f
greensnapback0229 <smdmim@gmail.com>
2025-07-30 00:44:32 +0900

    feat: ETF 상품정보 조회 응답 DTO 추가

---
+++ src/main/java/woojooin/planit/global/util/openData/dto/price/etf/ETFPriceRes.java
@@ -0,0 +1,79 @@
+package woojooin.planit.global.util.openData.dto.price.etf;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class ETFPriceRes {
+
+	private int numOfRows;
+	private int pageNo;
+	private int totalCount;
+	private Items items;
+
+	@Data
+	public static class Items {
+		private List<Item> item;
+	}
+
+	@Data
+	public static class Item {
+
+		@JsonProperty("basDt")
+		private String basDt;            // 기준일자
+
+		@JsonProperty("srtnCd")
+		private String srtnCd;           // 단축코드
+
+		@JsonProperty("isinCd")
+		private String isinCd;           // ISIN코드
+
+		@JsonProperty("itmsNm")
+		private String itmsNm;           // 종목명
+
+		@JsonProperty("clpr")
+		private String clpr;             // 종가
+
+		@JsonProperty("vs")
+		private String vs;               // 대비
+
+		@JsonProperty("fltRt")
+		private String fltRt;            // 등락률
+
+		@JsonProperty("nav")
+		private String nav;              // 순자산가치
+
+		@JsonProperty("mkp")
+		private String mkp;              // 시가
+
+		@JsonProperty("hipr")
+		private String hipr;             // 고가
+
+		@JsonProperty("lopr")
+		private String lopr;             // 저가
+
+		@JsonProperty("trqu")
+		private String trqu;             // 거래량
+
+		@JsonProperty("trPrc")
+		private String trPrc;            // 거래대금
+
+		@JsonProperty("mrktTotAmt")
+		private String mrktTotAmt;       // 시가총액
+
+		@JsonProperty("stLstgCnt")
+		private String stLstgCnt;        // 상장주식수
+
+		@JsonProperty("bssIdxIdxNm")
+		private String bssIdxIdxNm;      // 기초지수명
+
+		@JsonProperty("bssIdxClpr")
+		private String bssIdxClpr;       // 기초지수 종가
+
+		@JsonProperty("nPptTotAmt")
+		private String nPptTotAmt;       // 순자산총액
+	}
+}
