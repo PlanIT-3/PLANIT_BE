d76a3c6a99ca510101b3fc541db839b517612bfa
bronze_yun_4521 <ehddbs452100@naver.com>
2025-07-30 10:23:16 +0900

    feat: #13 행동 템플릿 엔티티 생성

---
+++ src/main/java/woojooin/planit/domain/object/action/domain/ActionType.java
@@ -0,0 +1,25 @@
+package woojooin.planit.domain.object.action.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ActionType {
+
+	ISA("ISA"),
+	DEPOSIT("예적금"),
+	;
+
+	private final String description;
+
+	public static ActionType fromCode(String actionType) {
+		for (ActionType type : ActionType.values()) {
+			if (type.getDescription().equals(actionType)) {
+				return type;
+			}
+		}
+
+		return null;
+	}
+}
