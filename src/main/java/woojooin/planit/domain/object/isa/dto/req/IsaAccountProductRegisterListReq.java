394717a0bbc2cf337e659539c86ec327405b2fdf
bronze_yun_4521 <ehddbs452100@naver.com>
2025-07-30 15:46:22 +0900

    refactor: #13 에러 오류 코드 추가

--- src/main/java/woojooin/planit/domain/object/isa/dto/req/IsaAccountProductRegisterListReq.java
+++ src/main/java/woojooin/planit/domain/object/isa/dto/req/IsaAccountProductRegisterListReq.java
@@ -2,6 +2,10 @@ package woojooin.planit.domain.object.isa.dto.req;
 
 import java.util.List;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,5 +15,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class IsaAccountProductRegisterListReq {
 
+	@NotNull(message = "상품 목록은 필수입니다.")
+	@NotEmpty(message = "등록할 상품 목록이 비어있습니다.")
+	@Valid
 	private List<IsaAccountProductRegisterReq> isaAccountProductRegisterReqs;
 }
