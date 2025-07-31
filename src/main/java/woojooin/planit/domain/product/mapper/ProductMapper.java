7a95b5b6c6eff456f5a791fb2cd0b36b32f822ad
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-07-31 17:13:34 +0900

    feat: 상품 매퍼 인터페이스 수정(#28)

--- src/main/java/woojooin/planit/domain/product/mapper/ProductMapper.java
+++ src/main/java/woojooin/planit/domain/product/mapper/ProductMapper.java
@@ -1,7 +1,11 @@
 package woojooin.planit.domain.product.mapper;
 
+import java.util.List;
+
 import woojooin.planit.domain.product.domain.Product;
 
 public interface ProductMapper {
 	void insertProduct(Product product);
+	void deleteAll();
+	List<Product> selectByRiskLevel(String riskLevel);
 }
