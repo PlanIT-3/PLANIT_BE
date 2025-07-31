e501337b4e1193d10664510b6ff2622f8c17b902
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-07-31 17:21:48 +0900

    feat: 상품 추천 서비스 구현 및 예외 처리 추가(#28)

--- src/main/java/woojooin/planit/domain/product/service/ProductService.java
+++ src/main/java/woojooin/planit/domain/product/service/ProductService.java
@@ -1,11 +1,15 @@
 package woojooin.planit.domain.product.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import woojooin.planit.domain.product.domain.Product;
 import woojooin.planit.domain.product.mapper.ProductMapper;
+import woojooin.planit.global.exception.BusinessException;
+import woojooin.planit.global.response.ResponseCode;
 import woojooin.planit.global.util.openData.OpenApiUtil;
 import woojooin.planit.global.util.openData.dto.OpenApiResponse;
 import woojooin.planit.global.util.openData.dto.price.etf.ETFPriceRes;
@@ -30,6 +34,20 @@ public class ProductService {
 			mapper.insertProduct(product);
 		}
 	}
+
+	public void deleteAllProducts() {
+		mapper.deleteAll();
+	}
+
+
+	public List<Product> recommendProduct(String riskLevel) {
+		List<Product> products = mapper.selectByRiskLevel(riskLevel);
+		if (products == null || products.isEmpty()) {
+			throw new BusinessException(ResponseCode.ISA_PRODUCT_NOT_FOUND);
+		}
+		return products;
+	}
+
 
 	private boolean isValidResponse(OpenApiResponse<ETFPriceRes> response) {
 		return response != null &&
