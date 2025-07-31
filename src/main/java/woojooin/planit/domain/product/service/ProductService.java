aeacde97028886c22f5770b3497f913ee55ad175
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-07-31 10:28:52 +0900

    refactor: ETF 응답 null 체크 로직 메서드 분리(#20)

--- src/main/java/woojooin/planit/domain/product/service/ProductService.java
+++ src/main/java/woojooin/planit/domain/product/service/ProductService.java
@@ -20,22 +20,24 @@ public class ProductService {
 	public void fetchAndSaveProducts(){
 		OpenApiResponse<ETFPriceRes> response = openApiUtil.getETFPriceInfo();
 
-		if (response == null ||
-			response.getResponse() == null ||
-			response.getResponse().getBody() == null ||
-			response.getResponse().getBody().getItems() == null ||
-			response.getResponse().getBody().getItems().getItem() == null) {
-
+		if (!isValidResponse(response)) {
 			System.out.println("ETF API 응답이 비어 있습니다.");
 			return;
 		}
 
-		// 데이터 변환 및 저장
 		for (ETFPriceRes.Item item : response.getResponse().getBody().getItems().getItem()) {
 			Product product = ETFPriceRes.mapItemToProduct(item);
 			log.info("product={}", product);
 			mapper.insertProduct(product);
 		}
+	}
+
+	private boolean isValidResponse(OpenApiResponse<ETFPriceRes> response) {
+		return response != null &&
+			response.getResponse() != null &&
+			response.getResponse().getBody() != null &&
+			response.getResponse().getBody().getItems() != null &&
+			response.getResponse().getBody().getItems().getItem() != null;
 	}
 
 
