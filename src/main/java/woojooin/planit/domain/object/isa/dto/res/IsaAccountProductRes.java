9326c542fbbef32bdc9a34a182b99cff023741d0
bronze_yun_4521 <ehddbs452100@naver.com>
2025-08-07 19:42:58 +0900

    fix: #41 object를 goal로 변

--- src/main/java/woojooin/planit/domain/object/isa/dto/res/IsaAccountProductRes.java
+++ src/main/java/woojooin/planit/domain/object/isa/dto/res/IsaAccountProductRes.java
@@ -5,7 +5,6 @@ import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import woojooin.planit.domain.product.domain.ProductTypeCode;
 
 @Data
 @NoArgsConstructor
@@ -13,10 +12,8 @@ import woojooin.planit.domain.product.domain.ProductTypeCode;
 public class IsaAccountProductRes {
 
 	private Long memberProductId;
-	private ProductTypeCode productTypeCode;
 	private BigDecimal presentAmount;
 	private Integer quantity;
 	private String itemName;
-	private String itemCode;
 	private boolean checked;
 }
