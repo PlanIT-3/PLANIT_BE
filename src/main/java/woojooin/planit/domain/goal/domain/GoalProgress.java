741ca7c0fc3d74f135fdd330a2d5f0def3518ed1
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-08-08 14:25:14 +0900

    feat: GoalProgress 엔티티 생성(#48)

--- src/main/java/woojooin/planit/domain/goal/domain/GoalProgress.java
+++ src/main/java/woojooin/planit/domain/goal/domain/GoalProgress.java
@@ -1,4 +1,13 @@
 package woojooin.planit.domain.goal.domain;
 
+import java.time.LocalDateTime;
+
 public class GoalProgress {
+	private int dailyGoalProgressId;  // daily_goal_progress_id
+	private int goalId;                // goal_id
+	private double isaProgress;        // isa_progress
+	private double depositProgress;    // deposit_progress
+	private LocalDateTime progressDate; // progress_date
+	private LocalDateTime createdAt;    // created_at
+	private LocalDateTime updatedAt;    // updated_at
 }
