d0b5d7192bbcd05d85fd7ee08c4cd85e1dccc61e
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-08-08 14:22:22 +0900

    feat: 목표 진행 추이 조회 메서드 정의#(48)

--- src/main/java/woojooin/planit/domain/goal/mapper/GoalMapper.java
+++ src/main/java/woojooin/planit/domain/goal/mapper/GoalMapper.java
@@ -3,17 +3,25 @@ package woojooin.planit.domain.goal.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
 import woojooin.planit.domain.goal.domain.Goal; // 변경된 Goal VO 임포트
+import woojooin.planit.domain.goal.dto.GoalProgressGraphDTO;
 
 import java.util.List;
 
 @Mapper
 
 public interface GoalMapper {
-    int insertGoal(Goal goal);
-    Goal selectGoalById(@Param("objectId") Long objectId, @Param("memberId") Long memberId);
-    List<Goal> selectAllGoals(Long memberId);
-    int updateGoal(Goal goal);
-    int deleteGoal(@Param("objectId") Long objectId, @Param("memberId") Long memberId);
+	int insertGoal(Goal goal);
+
+	Goal selectGoalById(@Param("objectId") Long objectId, @Param("memberId") Long memberId);
+
+	List<Goal> selectAllGoals(Long memberId);
+
+	int updateGoal(Goal goal);
+
+	int deleteGoal(@Param("objectId") Long objectId, @Param("memberId") Long memberId);
+
+	List<GoalProgressGraphDTO> selectGoalProgressByGoalId(int goalId);
 
 }
