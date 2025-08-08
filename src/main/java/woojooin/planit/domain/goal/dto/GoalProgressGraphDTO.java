686f309b2dfbff1e7c244c8178331f9a7a88ec21
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-08-08 14:19:59 +0900

    feat: 목표 진행 추이 응답용 DTO(#48)

--- src/main/java/woojooin/planit/domain/goal/dto/GoalProgressGraphDTO.java
+++ src/main/java/woojooin/planit/domain/goal/dto/GoalProgressGraphDTO.java
@@ -1,4 +1,19 @@
 package woojooin.planit.domain.goal.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GoalProgressGraphDTO {
+	private LocalDateTime progressDate;
+	private double isaProgress;
+	private double depositProgress;
+
 }
+
