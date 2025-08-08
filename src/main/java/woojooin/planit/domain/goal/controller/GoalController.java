b1f4b0d23e46adb8ccf8013d5f7bd230e753a156
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-08-08 14:57:32 +0900

    fix: controller 오류 수정

--- src/main/java/woojooin/planit/domain/goal/controller/GoalController.java
+++ src/main/java/woojooin/planit/domain/goal/controller/GoalController.java
@@ -7,7 +7,6 @@ import woojooin.planit.global.exception.BusinessException;
 import woojooin.planit.global.response.Response;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
