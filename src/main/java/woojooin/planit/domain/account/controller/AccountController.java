c96115529a1a21c85b4243557d383dbd98b8035e
bronze_yun_4521 <ehddbs452100@naver.com>
2025-08-08 22:40:04 +0900

    refactor: #51 Security 적용

--- src/main/java/woojooin/planit/domain/account/controller/AccountController.java
+++ src/main/java/woojooin/planit/domain/account/controller/AccountController.java
@@ -1,6 +1,7 @@
 package woojooin.planit.domain.account.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,20 +13,22 @@ import lombok.RequiredArgsConstructor;
 import woojooin.planit.domain.account.dto.res.BalanceListRes;
 import woojooin.planit.domain.account.service.AccountService;
 import woojooin.planit.global.response.Response;
+import woojooin.planit.global.security.CustomUserDetails;
 
 @RestController
 @RequestMapping("/auth/api/account")
 @RequiredArgsConstructor
-@Api(value = "계좌 API" ,description = "계좌 설정 및 조회 관련 API ")
+@Api(value = "계좌 API", description = "계좌 설정 및 조회 관련 API ")
 public class AccountController {
 
 	private final AccountService accountService;
 
 	@GetMapping("/{period}")
-	@ApiOperation(value = "기간별 잔고 조회" , notes = "사용자의 기간별 잔고를 조회합니다. period: day(일주일), week(6주), month(6개월)" )
-	public ResponseEntity<Response<BalanceListRes>> getAccountBalanceForDate(@PathVariable String period) {
+	@ApiOperation(value = "기간별 잔고 조회", notes = "사용자의 기간별 잔고를 조회합니다. period: day(일주일), week(6주), month(6개월)")
+	public ResponseEntity<Response<BalanceListRes>> getAccountBalanceForDate(
+		@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable String period) {
 
-		BalanceListRes balance = accountService.getAccountBalanceForDate(1L, period);
+		BalanceListRes balance = accountService.getAccountBalanceForDate(customUserDetails.getId(), period);
 		return ResponseEntity.ok(Response.ok(balance));
 	}
 }
