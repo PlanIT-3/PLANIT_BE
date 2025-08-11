9a3b6436cdefba04327f69ffa530078850d136bf
greensnapback0229 <smdmim@gmail.com>
2025-08-11 23:44:46 +0700

    feat: openAi 유틸 추가

---
+++ src/main/java/woojooin/planit/global/security/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package woojooin.planit.global.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+	
+}
---
+++ src/main/java/woojooin/planit/global/util/openai/dto/keywordDto.java
@@ -0,0 +1,4 @@
+package woojooin.planit.global.util.openai.dto;
+
+public record keywordDto(String investKeyword) {
+}
