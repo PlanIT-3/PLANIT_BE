18fbb166771a52b7c09212c0b443d876379b2736
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-08-12 16:20:00 +0900

    fix: 한도 금액 변경(#83)

--- src/main/java/woojooin/planit/domain/report/service/ReportService.java
+++ src/main/java/woojooin/planit/domain/report/service/ReportService.java
@@ -59,7 +59,7 @@ public class ReportService {
 
     public IsaTaxSavingStatusDTO getTaxSavingStatus(Long memberId) {
         String isaType = reportMapper.getIsaType(memberId);
-        int maxTaxSavingLimit = "RURAL".equals(isaType) ? 400 : 200;
+        int maxTaxSavingLimit = "RURAL".equals(isaType) ? 4000000 : 2000000;
         int currentTaxSaving = reportMapper.getCurrentTaxSaving(memberId);
 
         IsaTaxSavingStatusDTO dto = new IsaTaxSavingStatusDTO();
