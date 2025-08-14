886992f2cd58186a04f3105931188a88c33f1c3c
kayoii <132435321+pgy8404@users.noreply.github.com>
2025-08-14 16:31:21 +0900

    fix:totalAmount 수정

--- src/main/java/woojooin/planit/domain/goal/service/GoalSettingService.java
+++ src/main/java/woojooin/planit/domain/goal/service/GoalSettingService.java
@@ -16,6 +16,7 @@ import woojooin.planit.domain.goal.dto.GoalDetailResponseDto;
 import woojooin.planit.domain.goal.dto.GoalRequestDto;
 import woojooin.planit.domain.goal.dto.GoalProgressGraphDTO;
 import woojooin.planit.domain.goal.dto.DailyGoalProgressResponse;
+import woojooin.planit.domain.goal.dto.res.GoalDepositResponse;
 import woojooin.planit.domain.goal.mapper.GoalMapper;
 import woojooin.planit.domain.goal.isa.dto.res.IsaAccountProductRes;
 import woojooin.planit.domain.goal.deposit.dto.res.DepositAccountRes;
@@ -50,7 +51,7 @@ public class GoalSettingService {
 		}
 		//  isa , 예적금 상품 목록 조회
 		List<IsaAccountProductRes> isaItems = goalMapper.findAllocatedIsaByGoal(memberId, goalId);
-		List<DepositAccountRes> depositAccounts = goalMapper.findAllocatedDepositByGoal(memberId, goalId);
+		List<GoalDepositResponse> depositAccounts = goalMapper.findAllocatedDepositByGoal(memberId, goalId);
 
 		long totalIsaAmount = isaItems.stream()
 			.mapToLong(item -> item.getPresentAmount()
@@ -103,7 +104,7 @@ public class GoalSettingService {
 		}
 		return goals.stream().map(goal -> {
 			List<IsaAccountProductRes> isaList = goalMapper.findAllocatedIsaByGoal(memberId, goal.getGoalId());
-			List<DepositAccountRes> depositList = goalMapper.findAllocatedDepositByGoal(memberId, goal.getGoalId());
+			List<GoalDepositResponse> depositList = goalMapper.findAllocatedDepositByGoal(memberId, goal.getGoalId());
 
 			long totalIsaAmount =
 				isaList.stream()
