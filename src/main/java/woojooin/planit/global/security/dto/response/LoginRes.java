e765d5c0b17550b1c1b9254e6e00778ca0979c05
seungwon-yu <y123587979@gmail.com>
2025-08-19 22:44:15 +0900

    fix:fcm 토큰 저장 구현

--- src/main/java/woojooin/planit/global/security/dto/response/LoginRes.java
+++ src/main/java/woojooin/planit/global/security/dto/response/LoginRes.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 @AllArgsConstructor
 @RequiredArgsConstructor
 public class LoginRes {
+    private Long memberId;  // memberId 추가
     private String email;
     private String password;
     private String username;
