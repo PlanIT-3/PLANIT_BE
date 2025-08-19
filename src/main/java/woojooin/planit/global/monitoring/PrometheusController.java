99385165598f8f7d77137615e90135d2801bd936
greensnapback0229 <smdmim@gmail.com>
2025-08-20 02:39:11 +0900

    feat: 모니터링 레지스트리 컨트롤러 restController로 변경

--- src/main/java/woojooin/planit/global/monitoring/PrometheusController.java
+++ src/main/java/woojooin/planit/global/monitoring/PrometheusController.java
@@ -1,13 +1,15 @@
 package woojooin.planit.global.monitoring;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-@Controller
+@Slf4j
+@RestController
 @RequiredArgsConstructor
 public class PrometheusController {
 
@@ -16,6 +18,7 @@ public class PrometheusController {
 	@GetMapping(value = "/test/prometheus", produces = "text/plain; version=0.0.4; charset=utf-8")
 	@ResponseBody
 	public String scrape() {
+		log.info("prometheus scrape");
 		return registry.scrape();
 	}
 
