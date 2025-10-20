26198ddde99c8b38ac9edeba26efc942a95038ae
greensnapback0229 <smdmim@gmail.com>
2025-10-21 03:08:20 +0900

    feat: action 도메인 mp 조회 기능 추가

--- src/main/java/woojooin/planit/domain/goal/action/domain/Action.java
+++ src/main/java/woojooin/planit/domain/goal/action/domain/Action.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import woojooin.planit.domain.member.domain.MemberProduct;
 
 @Data
 @AllArgsConstructor
@@ -20,6 +21,7 @@ public class Action {
 	private String accountNumber;
 
 	private Long memberProductId;
+	private MemberProduct memberProduct;
 
 	private Integer allocatedRate;
 
